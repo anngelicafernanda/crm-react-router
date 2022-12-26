eb2559a7efd464448fec9c95b492f82af1909a3e
anngelicafernanda <devanngelc@gmail.com>
2022-12-26 16:11:55 -0300

    Mostrando clientes

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cliente = ({cliente}) => {
+  const { nombre, empresa, email, telefono, id } = cliente;
+  return (
+    <tr>
+      <td className="p-6">{nombre}</td>
+      
+    </tr>
+  );
+};
+
+export default Cliente;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
 
-const index = () => {
-  return (
-    <div>
-      Clientes
-    </div>
-  )
+export function loader() {
+  const clientes = [
+    {
+      id: 1,
+      nombre: "Juan",
+      telefono: 102013313,
+      email: "juan@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 2,
+      nombre: "Karen",
+      telefono: 138198313,
+      email: "karen@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 3,
+      nombre: "Josue",
+      telefono: 31983913,
+      email: "josue@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 4,
+      nombre: "Miguel",
+      telefono: 319381983,
+      email: "miguel@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 5,
+      nombre: "Pedro",
+      telefono: 1398198938,
+      email: "pedro@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+  ];
+
+  return clientes;
 }
 
-export default index
+const Index = () => {
+  const clientes = useLoaderData();
 
+  return (
+    <>
+      <h1 className="font-block text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+      {clientes.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead>
+            <tr>
+              <th className="p-2">Cliente</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <Cliente cliente={cliente} key={cliente.id} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10">No hay Clientes</p>
+      )}
+    </>
+  );
+};
+
+export default Index;
