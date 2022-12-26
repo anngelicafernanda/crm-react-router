9f0689b33f836e2cf2c895d1997e341484bd9e3a
anngelicafernanda <devanngelc@gmail.com>
2022-12-26 14:35:25 -0300

    Configuracion del proyecto con react router

---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      Clientes
+    </div>
+  )
+}
+
+export default index
+
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NuevoCliente() {
+  return (
+    <div>
+      Nuevo Cliente
+    </div>
+  )
+}
+
+export default NuevoCliente
